25bfeb6f65dba8855bc9fa076b10f52e777c9618
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-11-15 20:21:29 +0530

    feat(waitlist): add social icons

--- packages/waitlist/components/layout/Footer.tsx
+++ packages/waitlist/components/layout/Footer.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 
+import UilTwitter from "@iconscout/react-unicons/icons/uil-twitter-alt";
+import UilGithub from "@iconscout/react-unicons/icons/uil-github-alt";
+import UilInstagram from "@iconscout/react-unicons/icons/uil-instagram";
+
 const Footer: React.FC = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className="flex items-center gap-2">
+      <Social
+        Icon={UilGithub}
+        href="https://github.com/polenhq"
+        title="Github"
+      />
+
+      <Social
+        Icon={UilTwitter}
+        href="https://twitter.com/polenhq"
+        title="Twitter"
+      />
+
+      <Social
+        Icon={UilInstagram}
+        href="https://instagram.com/polenhq"
+        title="Instagram"
+      />
+    </footer>
+  );
+};
+
+const Social = ({ Icon, href, title }) => {
+  return (
+    <a href={href} title={title}>
+      <Icon className="text-white hover:opacity-[0.6]" size="25" />
+    </a>
+  );
 };
 
 export default Footer;
